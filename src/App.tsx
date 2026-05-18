f697b2f9e129098600bfa3c255c1346456fbec13
Shawheen Azizi <fezzman@gmail.com>
2026-05-17 22:16:28 -0700

    fix: use actual photo of yasmine instead of DNS error screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ function App() {
               <div style={{
                 position: 'absolute',
                 top: 0, left: 0, right: 0, bottom: 0,
-                backgroundImage: 'url(/hero-yasmine.png)',
+                backgroundImage: 'url(/hero-yasmine.jpg)',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center 30%',
                 zIndex: -2,
